0edc3bebed534a92fae59b3f96744f258e92090a
Sam Mortimer <sam@mortimer.me.uk>
2014-11-25 14:18:22 -0800

    AppOps: show/hide app type and allow/deny counters [1/2]

--- src/com/android/settings/applications/AppOpsDetails.java
+++ src/com/android/settings/applications/AppOpsDetails.java
@@ -129,6 +129,8 @@ public class AppOpsDetails extends InstrumentedFragment {
                 }
                 ((TextView)view.findViewById(R.id.op_name)).setText(
                         entry.getSwitchText(mState));
+                ((TextView)view.findViewById(R.id.op_counts)).setText(
+                        entry.getCountsText(res));
                 ((TextView)view.findViewById(R.id.op_time)).setText(
                         entry.getTimeText(res, true));
                 Switch sw = (Switch)view.findViewById(R.id.switchWidget);
