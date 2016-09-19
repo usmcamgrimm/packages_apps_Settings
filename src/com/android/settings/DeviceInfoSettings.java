b7ee1a2e2cae7b3a12f622a9d4725b392fae4809
beanstown106 <nbenis106@gmail.com>
2016-09-19 01:00:23 -0400

    Dynamically define currently installed vendor.img version *parse the vendor fingerprint for the vendor build id *automatically remove on devices without a vendor image
    Thanks to @dwitherell for the help with the parsing logic :)
    
    8.0 edits:
    *Updated to a controller
    
    Change-Id: I7bb504f9018349e4e5acd8043fe12196f121eea4

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -39,6 +39,7 @@ import com.android.settings.deviceinfo.RegulatoryInfoPreferenceController;
 import com.android.settings.deviceinfo.SELinuxStatusPreferenceController;
 import com.android.settings.deviceinfo.SafetyInfoPreferenceController;
 import com.android.settings.deviceinfo.SecurityPatchPreferenceController;
+import com.android.settings.deviceinfo.VendorVersionPreferenceController;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
 
@@ -129,6 +130,7 @@ public class DeviceInfoSettings extends DashboardFragment implements Indexable {
         controllers.add(new FccEquipmentIdPreferenceController(context));
         controllers.add(new SELinuxStatusPreferenceController(context));
         controllers.add(new SafetyInfoPreferenceController(context));
+        controllers.add(new VendorVersionPreferenceController(context));
         return controllers;
     }
 
